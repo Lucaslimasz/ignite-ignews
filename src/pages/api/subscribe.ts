3c00d4f37d9f544ca9fcf5a11efe3753ce04fcde
Lucas Lima <lucaslima@Lucass-MacBook-Air.local>
2021-12-03 01:03:21 -0300

    config subscribe

--- src/pages/api/subscribe.ts
+++ src/pages/api/subscribe.ts
@@ -1,14 +1,42 @@
 import { NextApiRequest, NextApiResponse } from "next";
+import { query as q} from 'faunadb'
 import { getSession } from "next-auth/client";
+import { fauna } from "../../services/fauna";
 import { stripe } from "../../services/stripe";
+
+interface User {
+  ref: {
+    id: string;
+  }
+}
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   if (req.method === "POST") {
     const session = await getSession({ req });
 
+    const user = await fauna.query<User>(
+      q.Get(
+        q.Match(
+          q.Index('user_by_email'),
+          q.Casefold(session.user.email),
+        )
+      )
+    )
+
     const stripeCustomer = await stripe.customers.create({
       email: session.user.email,
     });
+
+    await fauna.query(
+      q.Update(
+        q.Ref(q.Collection('users'), user.ref.id),
+        {
+          data: {
+            stripe_custormer_id: stripeCustomer.id
+          }
+        }
+      )
+    )
 
     const stripeCheckoutSession = await stripe.checkout.sessions.create({
       customer: stripeCustomer.id,
